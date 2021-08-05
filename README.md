e3a3b70f5d374e16eb7ee3c81e4866ccebefa42d
凉风有信 <3042146237@qq.com>
2021-08-05 22:05:54 +0800

    bs样式引入，column组件书写

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ### Vue3+TS 仿知乎项目
 
 写一个项目进行 vue3 及 TS 的进一步学习
+
+安装 bootstrap：
+
+```base
+npm install bootstrap@next --save
+```
--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,46 @@
 <template>
-主页面
+  <div class="container">
+    <column-list :list="list"></column-list>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from './components/Column List.vue'
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1的专栏',
+    description: '13422124234232',
+    avatar: 'http://www.dell-lee.com/imgs/vue3/%E8%B6%85%E5%B8%82.png'
+  },
+  {
+    id: 2,
+    title: 'test2的专栏',
+    description: '13422124234232'
+    // avatar: 'http://www.dell-lee.com/imgs/vue3/%E8%B6%85%E5%B8%82.png'
+  },
+  {
+    id: 3,
+    title: 'test1的专栏',
+    description: '13422124234232345555555555555555555555555555',
+    avatar: 'http://www.dell-lee.com/imgs/vue3/%E8%B6%85%E5%B8%82.png'
+  },
+  {
+    id: 4,
+    title: 'test2的专栏',
+    description: '13422124234232',
+    avatar: 'http://www.dell-lee.com/imgs/vue3/%E8%B6%85%E5%B8%82.png'
+  }
+]
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+  components: { ColumnList },
+  setup () {
+    return { list: testData }
+  }
 })
 </script>
 
